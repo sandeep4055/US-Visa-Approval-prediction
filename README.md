4720af2bc9fc7f5893e41c709f4c0fd32d98707c
Sandeep Illuri <illuri.sandeep5454@gmail.com>
2024-04-30 01:47:02 +0530

    project setup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # US-Visa-Approval-prediction
+
+
+```bash 
+conda create -n visa python=3.8 -y
+```
+```bash 
+conda activate bash
+```
+```bash 
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sandeep Illuri",
+    author_email="illuri.sandeep5454@gmail.com",
+    packages=find_packages()
+)
